70205190d41d515f615d966d45342050495ad795
Chelcie Tjoeng <tjoeng@atrihub.io>
2025-04-14 11:25:33 -0700

    feat(training): TDD Tutorial
    Complete Chapter 2: Extending Our Functional Test Using the unittest Module [ICTRAINING-1976]

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,59 @@
+import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert "Congratulations!" in browser.title
-print("OK")
+class NewVisitorTest(unittest.TestCase):  
+    def setUp(self):  
+        self.browser = webdriver.Firefox()  
+
+    def tearDown(self):  
+        self.browser.quit()
+
+    def test_can_start_a_todo_list(self):  
+        # Edith has heard about a cool new online to-do app.
+        self.browser.get("http://localhost:8000")  
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-Do", header_text)
+
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(
+            inputbox.get_attribute("placeholder"),
+            "Enter a to-do item"
+        )
+
+        # She types "Buy peacock feathers" into a text box
+        inputbox.send_keys("Buy peacock feathers")
+
+        # When she hits enter, the page updates
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)  # temporary wait for page to update
+
+        # Now the page lists "1: Buy peacock feathers" as an item in a to-do list
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+
+        # There is still a text box inviting her to add another item
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # The page updates again, and now shows both items on her list
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows])
+
+        # Satisfied, she goes back to sleep
+
+
+if __name__ == "__main__":  
+    unittest.main()
